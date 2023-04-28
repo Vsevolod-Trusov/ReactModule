51af53063cc5e243bb14a75bb5f2eb8ab5b950ae
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-04-28 15:38:01 +0300

    feat: implement material ui with styled components
    feat: add button from material ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button, styled } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Button variant='contained'>Hello World</Button>
     </div>
   );
 }
