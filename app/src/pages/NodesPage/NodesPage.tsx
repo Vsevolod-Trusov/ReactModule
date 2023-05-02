e44b75c3f3a6c5434c3cf41abedcc8a548bd12c9
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-02 18:25:04 +0300

    feat: implement NodePage

---
+++ app/src/pages/NodesPage/NodesPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NodeList from '../../components/composites/NodeList/NodeList';
+
+const NodesPage = () => {
+  return (
+      <NodeList />
+  )
+}
+
+export default NodesPage
