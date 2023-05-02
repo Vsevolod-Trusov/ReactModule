8cdb523402de38f9ef76426916353727d7939f57
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-02 18:25:27 +0300

    feat: implement global styles

---
+++ app/src/styles/global-styles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+import MontserratWoff from '../fonts/Montserrat/Montserrat.woff';
+import MontserratWoff2 from '../fonts/Montserrat/Montserrat.woff2';
+
+export const globalStyles = () => {
+  return createGlobalStyle`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      
+      @font-face {
+          font-family: 'Montserrat';
+          src: local('Montserrat'),
+          url(${MontserratWoff2}) format('woff2'),
+          url(${MontserratWoff}) format('woff')
+      }
+
+    `;
+};
+
