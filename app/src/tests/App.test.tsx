e44582b3da48c140d2eab3db80fad62498533f75
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-03 16:14:29 +0300

    refactor: add index for imports

--- app/src/tests/App.test.tsx
+++ app/src/tests/App.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+
+import {
+  render,
+  screen,
+  App
+} from './index'
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ app/src/tests/index.ts
@@ -0,0 +1,2 @@
+export { render, screen } from '@testing-library/react';
+export { App } from '../pages/App/App';
