b666076cae3be6ea094e33ecdf03b4b27c97ca3f
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-03 16:14:57 +0300

    fix: changes after index.ts

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import { router } from './config/routes/router';
-import { globalStyles } from './styles/global-styles';
+import {
+  RouterProvider,
+  globalStyles,
+  router,
+} from './pages/Index';
 
 const Global = globalStyles();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-    <>
-      <Global/>
-      <RouterProvider router={router} />
-    </>
+  <>
+    <Global />
+    <RouterProvider router={router} />
+  </>,
 );
 
