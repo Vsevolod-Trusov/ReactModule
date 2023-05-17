5d5259ee2ca651283144284c7814e84961e78a06
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-17 20:39:33 +0300

    fix: change link

--- app/src/components/Navbar/components/Link/LinkContainer.tsx
+++ app/src/components/Navbar/components/Link/LinkContainer.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import Link from './Link';
 import { NODES } from '../../../../pages/constants';
+import { ROUTE } from '../../../../config/routes/routes';
 
 const LinkContainer = () => {
   const navigate = useNavigate();
 
   return (
-    <Link onClick={() => navigate('/nodes')}
+    <Link onClick={() => navigate(ROUTE.NOTES)}
           name={NODES} />
   );
 };
