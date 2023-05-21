9bf70b695ce9f6fa6538ad857ce126a9fe2b781c
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-21 11:29:37 +0300

    feat: implement custom title

---
+++ app/src/components/Title/Title.tsx
@@ -0,0 +1,13 @@
+import React, { FC, PropsWithChildren } from 'react';
+import { StyledTitle } from './styled';
+import { TypographyProps } from '@mui/material/Typography/Typography';
+
+const Title: FC<PropsWithChildren<TypographyProps>> = (props) => {
+  return (
+    <StyledTitle variant={props.variant}>
+      {props.children}
+    </StyledTitle>
+  );
+};
+
+export default Title;
---
+++ app/src/components/Title/styled.tsx
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography/Typography';
+import styled from 'styled-components';
+
+const StyledTitle = styled(Typography)({
+
+});
+
+export { StyledTitle };
