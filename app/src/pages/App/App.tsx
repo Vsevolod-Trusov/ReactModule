4e51f6d0af8a989b35cfcd61d011b9d1b32453df
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-21 11:30:56 +0300

    fix: use absolute path

--- app/src/pages/App/App.tsx
+++ app/src/pages/App/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import { Outlet } from 'react-router-dom';
 
+import {default as NavbarContainer} from 'components/Navbar/index'
+
 import { StyledApp, StyledNavbarWrapper, StyledOutlet } from './styled';
-import {default as NavbarContainer} from '../../components/Navbar/index'
 
 const App = () => {
   return (
