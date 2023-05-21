445917011e6a650022991d0dd7bcced6c5564412
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-21 11:32:05 +0300

    fix: add colors and constants

--- app/src/pages/constants.ts
+++ app/src/pages/constants.ts
@@ -1,2 +1,12 @@
-export const MENU = 'Menu'
-export const NODES = 'Nodes'
+export const MENU = 'Menu';
+
+export enum ROUTE_LABELS {
+  NOTES = 'Notes',
+  SIGNIN = 'Sign In',
+  SIGNUP = 'Sign Up'
+}
+
+export const EMAIL_FIELD = 'email';
+export const PASSWORD_FIELD = 'password';
+
+export const MIN_DATE = '1950-01-01'
