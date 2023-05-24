aa4ff05b45a4d99f2003e4e84d2e847ea0f9796a
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-24 16:52:40 +0300

    fix: change navbar
    add liks(shared and about)
    add logout link

--- app/src/components/Navbar/components/Link/LinkContainer.tsx
+++ app/src/components/Navbar/components/Link/LinkContainer.tsx
@@ -1,14 +1,26 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import Link from './Link';
-import { ILinkContainer } from '../../types';
+import { ILinkContainer } from 'components/Navbar/types';
+import { ROUTE } from 'config/routes/routes';
 
-const LinkContainer:FC<ILinkContainer> = ({name, route}) => {
+import Link from './Link';
+
+const LinkContainer: FC<ILinkContainer> = ({ name, route, isExit }) => {
   const navigate = useNavigate();
 
+  if (isExit) {
+    return (
+      <Link onClick={() => {
+        window.localStorage.removeItem('email');
+        navigate(ROUTE.MAIN)
+      }}
+            name={name} />
+    );
+  }
+
   return (
-    <Link onClick={() => navigate(route)}
+    <Link onClick={() => navigate(route ? route : ROUTE.NOT_FOUND)}
           name={name} />
   );
 };
