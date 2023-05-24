1a19609ac17538e46728f8b281248c405b7d5a2e
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-24 16:53:41 +0300

    feat: add routes for share and about pages

--- app/src/config/routes/router.tsx
+++ app/src/config/routes/router.tsx
@@ -1,11 +1,15 @@
 import {createBrowserRouter, createRoutesFromElements, Navigate, Route} from 'react-router-dom';
 
-import { default as App } from '../../pages/App/index'
-import { default as NotesListContainer } from '../../pages/Notes/index'
-import { default as ErrorPageContainer } from "../../pages/Error/index";
-import { default as EditNoteContainer } from "../../pages/Notes/components/SelectedNote/components/EditNoteForm/index";
-import { default as SignInContainer } from "../../pages/SignIn/index";
-import { default as SignUpContainer } from "../../pages/SignUp/index";
+import { default as App } from 'pages/App/index'
+import { default as NotesListContainer } from 'pages/Notes/index'
+import { default as ErrorPageContainer } from "pages/Error/index";
+import { default as EditNoteContainer } from "pages/Notes/components/SelectedNote/components/EditNoteForm/index";
+import { default as SignInContainer } from "pages/SignIn/index";
+import { default as SignUpContainer } from "pages/SignUp/index";
+import { default as SharedNotesContainer } from "pages/SharedNotes/index";
+import { default as AboutContainer } from "pages/About/index";
+
+
 import { ROUTE } from './routes';
 
 
@@ -13,6 +17,8 @@ export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path={ROUTE.MAIN} element={<App/>}>
             <Route path={ROUTE.NOTES} element={<NotesListContainer/>}/>
+            <Route path={ROUTE.SHARED} element={<SharedNotesContainer/>}/>
+            <Route path={ROUTE.ABOUT} element={<AboutContainer/>}/>
             <Route path={ROUTE.NOTE} element={<EditNoteContainer/>}/>
             <Route path={ROUTE.SIGNIN} element={<SignInContainer/>}/>
             <Route path={ROUTE.SIGNUP} element={<SignUpContainer/>}/>
