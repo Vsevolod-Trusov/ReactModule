3e09dc4744d5fe4fcac5788cc60a161d5a1a8e78
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-24 16:58:05 +0300

    fix:: change main constants

--- app/src/pages/constants.ts
+++ app/src/pages/constants.ts
@@ -1,12 +1,13 @@
 export const MENU = 'Menu';
 
 export enum ROUTE_LABELS {
-  NOTES = 'Notes',
+  NOTES = 'My Notes',
+  SHARED = 'Shared notes',
+  ABOUT = 'About',
   SIGNIN = 'Sign In',
-  SIGNUP = 'Sign Up'
+  SIGNUP = 'Sign Up',
+  EXIT = 'Log out',
 }
 
 export const EMAIL_FIELD = 'email';
 export const PASSWORD_FIELD = 'password';
-
-export const MIN_DATE = '1950-01-01'
