8bbeb76571732feac44092a240322cb35e37f144
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-24 16:58:33 +0300

    fix: connect redux and react-query

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-
-import { default as globalStyles } from './styles/index';
-import { default as router } from './config/routes/index';
 import { ThemeProvider } from '@mui/material';
-import { theme } from './config/theme/theme.';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+
+import {default as store } from 'config/redux/store'
+import { default as router } from 'config/routes/index';
+import { theme } from 'config/theme/theme';
+import { default as globalStyles } from 'styles/index';
 
 const Global = globalStyles();
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -15,9 +20,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <Global />
-   <ThemeProvider theme={theme}>
-     <RouterProvider router={router} />
-   </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={client}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </>,
 );
 
