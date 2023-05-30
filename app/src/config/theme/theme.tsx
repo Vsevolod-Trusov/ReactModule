efe4ecf50301abde30d147c3368da922b8addf74
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-05-30 07:36:00 +0300

    fix: chage theme to use Montserrat font-fmily for TextField

--- app/src/config/theme/theme.tsx
+++ app/src/config/theme/theme.tsx
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat'
+  },
   components: {
     MuiTypography: {
       variants: [
@@ -16,15 +19,35 @@ export const theme = createTheme({
       ],
     },
     MuiFormControl: {
-          styleOverrides: {
-            root:{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              maxWidth: '100%',
-            }
-          }
-    }
+      styleOverrides: {
+        root: {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          maxWidth: '100%',
+        },
+      },
+    },
+    MuiTextField: {
+      variants: [
+        {
+          props: { variant: 'outlined' },
+          style: {
+            fontFamily: 'Montserrat',
+            fontSize: '1.55rem',
+            lineHeight: '1.75rem',
+          },
+        },
+      ],
 
-  }})
+      styleOverrides: {
+        root: {
+          fontFamily: 'Montserrat',
+          fontSize: '1.55rem',
+          lineHeight: '1.75rem',
+        },
+      },
+    },
+  },
+});
