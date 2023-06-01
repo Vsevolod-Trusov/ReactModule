6c54824966c840483c171ec673da4435910830a0
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-01 15:46:29 +0300

    fix: delete localstorage

--- app/src/pages/Notes/components/SelectedNote/components/EditNoteForm/EditNoteContainer.tsx
+++ app/src/pages/Notes/components/SelectedNote/components/EditNoteForm/EditNoteContainer.tsx
@@ -1,82 +1,82 @@
-import React, {ChangeEvent, FC, useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useDispatch, useSelector } from 'react-redux';
 
-import {TNote, TUpdateNote} from 'pages/Notes/types';
-import {ROUTE} from "config/routes/routes";
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'config/fetch_urls/fetch';
-
-import {IEditNodeProps, IHandleEditNote} from './types';
-import EditNoteForm from './EditNoteForm';
 import { QUERY_KEYS } from 'pages/constants';
+import { TNote, TUpdateNote } from 'pages/Notes/types';
+import { ROUTE } from 'config/routes/routes';
+import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'config/fetch_urls/fetch';
+import { selectNotes, setReduxNotes } from 'config/redux/slices/notes.slice';
+
+import { IEditNodeProps, IHandleEditNote } from './types';
+import EditNoteForm from './EditNoteForm';
 
 const EditNoteContainer: FC<IEditNodeProps> = () => {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const note = localStorage.getItem('selected') || undefined
-    const selectedNote: TNote = note ? JSON.parse(note) : undefined
-    const savedNotes = localStorage.getItem('notes');
-    const notes = savedNotes ? JSON.parse(savedNotes) : []
-    const queryClient = useQueryClient()
+  const note = localStorage.getItem('selected') || undefined;
+  const selectedNote: TNote = note ? JSON.parse(note) : undefined;
+  const savedNotes: TNote[] = useSelector(selectNotes);
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
 
-    const [description, setDescription] = useState<string>(selectedNote ? selectedNote?.description : '');
+  const [description, setDescription] = useState<string>(selectedNote ? selectedNote?.description : '');
 
-    const mutation = useMutation({
-        mutationFn: async (updatedNode: TUpdateNote) => (await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}/${selectedNote.id}`, {
-            method: FETCH_METHODS.PUT,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(updatedNode),
-        })),
-    })
-
-    const handleEditNote = ({description}: IHandleEditNote) => {
-
-        const selectedNoteIndex = notes.findIndex((item: TNote) => item.id === selectedNote.id)
-
-        const updatedNote = {
-            ...notes[selectedNoteIndex],
-            description: description,
-        };
-
-        const updatedNotes = [...notes];
-        updatedNotes[selectedNoteIndex] = updatedNote;
-
-        localStorage.setItem('notes', JSON.stringify(updatedNotes));
-
-        mutation.mutate({
-            description: description
-        });
-
-        queryClient.invalidateQueries({queryKey: [QUERY_KEYS.NOTES]})
-        navigate(ROUTE.NOTES)
-    };
-
-    const handleShareNote = () => {
-        mutation.mutate({
-            isShared: true
-        });
-        navigate(ROUTE.NOTES)
-    };
-
-
-    const handleSetDescription = ({target}: ChangeEvent<HTMLInputElement>) => {
-        setDescription(target.value)
+  const mutation = useMutation({
+    mutationFn: async (updatedNode: TUpdateNote) => (await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}/${selectedNote.id}`, {
+      method: FETCH_METHODS.PUT,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedNode),
+    })),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOTES] });
+      navigate(ROUTE.NOTES);
     }
+  });
 
-    useEffect(() => {
-        setDescription(selectedNote.description);
-    }, [selectedNote.description]);
+  const handleEditNote = ({ description }: IHandleEditNote) => {
 
-    return (
-        <EditNoteForm
-            note={selectedNote}
-            description={description}
-            handleEditNote={handleEditNote}
-            handleSetDescription={handleSetDescription}
-            handleShareNote={handleShareNote}
-        />
-    );
+    const selectedNoteIndex = savedNotes.findIndex((item: TNote) => item.id === selectedNote.id);
+    const updatedNotes = [...savedNotes];
+    const note = {...savedNotes[selectedNoteIndex]}
+
+    note.description = description;
+
+    updatedNotes[selectedNoteIndex] = note
+
+    dispatch(setReduxNotes(updatedNotes))
+
+    mutation.mutate({
+      description: description,
+    });
+  };
+
+  const handleShareNote = () => {
+    mutation.mutate({
+      isShared: true,
+    });
+    navigate(ROUTE.NOTES);
+  };
+
+  const handleSetDescription = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setDescription(target.value);
+  };
+
+  useEffect(() => {
+    setDescription(selectedNote.description);
+  }, [selectedNote.description]);
+
+  return (
+    <EditNoteForm
+      note={selectedNote}
+      description={description}
+      handleEditNote={handleEditNote}
+      handleSetDescription={handleSetDescription}
+      handleShareNote={handleShareNote}
+    />
+  );
 };
 
-export default EditNoteContainer
+export default EditNoteContainer;
