fd0a1aa4d72298b7f24855ea319ec97a50dbd4b2
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-01 15:42:39 +0300

    fix: use prettier

--- app/src/components/FormInput/FormInput.tsx
+++ app/src/components/FormInput/FormInput.tsx
@@ -1,16 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 import { FieldAttributes } from 'formik/dist/Field';
+import Box from '@mui/material/Box';
 
 import { StyledErrorMessage, StyledField } from './styled';
 
 const FormInput: FC<FieldAttributes<any>> = (props) => {
   return (
     <>
-      <StyledField {...props}/>
+      <StyledField {...props} />
       <StyledErrorMessage name={props.name} component={'div'} />
     </>
 
-)
-}
+  );
+};
 
-export default FormInput
+export default FormInput;
