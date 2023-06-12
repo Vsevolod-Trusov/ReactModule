662894422583b0b6c0f36968d5df766a0e450def
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-12 20:32:06 +0300

    fix: use prettier and  change index

--- app/src/components/FilterNotes/components/FilterForm.tsx
+++ app/src/components/FilterNotes/components/FilterForm.tsx
@@ -1,12 +1,15 @@
 import React, { FC, FormHTMLAttributes, PropsWithChildren } from 'react';
+
 import { StyledFilterForm } from './styled';
 
-const FilterForm:FC<PropsWithChildren<FormHTMLAttributes<HTMLFormElement>>> = (props) => {
+const FilterForm: FC<PropsWithChildren<FormHTMLAttributes<HTMLFormElement>>> = (
+  props,
+) => {
   return (
     <StyledFilterForm onSubmit={props.onSubmit}>
       {props.children}
     </StyledFilterForm>
-  )
-}
+  );
+};
 
-export default FilterForm
+export default FilterForm;
