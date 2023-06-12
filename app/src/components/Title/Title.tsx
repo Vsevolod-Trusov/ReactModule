4f7ad45c6f310694d40fbee9e61c1e1ef4f5deb1
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-12 14:28:13 +0300

    fix: change import

--- app/src/components/Title/Title.tsx
+++ app/src/components/Title/Title.tsx
@@ -1,13 +1,9 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { StyledTitle } from './styled';
-import { TypographyProps } from '@mui/material/Typography/Typography';
+import { TypographyProps } from '@mui/material/Typography';
 
 const Title: FC<PropsWithChildren<TypographyProps>> = (props) => {
-  return (
-    <StyledTitle variant={props.variant}>
-      {props.children}
-    </StyledTitle>
-  );
+  return <StyledTitle variant={props.variant}>{props.children}</StyledTitle>;
 };
 
 export default Title;
