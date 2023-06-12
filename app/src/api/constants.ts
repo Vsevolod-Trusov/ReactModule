25966c2f3d69516719fb9011499317d9a6cf75b2
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-12 20:28:34 +0300

    refactor: change fetch_urls file

---
+++ app/src/api/constants.ts
@@ -0,0 +1,15 @@
+export const MOCK_API_TOKEN = '64673847ba7110b663b25514';
+
+export const MOCK_API_ADDRESS = `https://${MOCK_API_TOKEN}.mockapi.io/api/`;
+
+export enum FETCH_URLS {
+  USERS = 'users/',
+  NOTES = 'notes/',
+}
+
+export enum FETCH_METHODS {
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  GET = 'GET',
+}
