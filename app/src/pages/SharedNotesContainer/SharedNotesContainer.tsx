84f76364561761c26e6c4162b480221571cd5c96
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-13 14:27:14 +0300

    fix: add api

--- app/src/pages/SharedNotesContainer/SharedNotesContainer.tsx
+++ app/src/pages/SharedNotesContainer/SharedNotesContainer.tsx
@@ -1,37 +1,21 @@
 import React, { FC } from 'react';
-import { useQuery } from '@tanstack/react-query';
 import { Navigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import { TNote } from 'pages/NoteList/types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { InfinityScroll, FilterNotes } from 'components/index';
 import { ROUTE } from 'config/constants/routes';
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'api/constants';
-import { setShared } from 'store/slices/notes.slice';
+import { useGetSharedNotes } from 'api/notes';
 import { selectFirstName } from 'store/slices/user.slice';
 
 const SharedNotesContainer: FC = () => {
   const firstname = useSelector(selectFirstName);
-  const dispatch = useDispatch();
 
   if (!window.localStorage.getItem('email') || !firstname) {
     return <Navigate to={ROUTE.SIGNIN} />;
   }
 
-  useQuery({
-    queryKey: ['notes'],
-    queryFn: async () =>
-      await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}`, {
-        method: FETCH_METHODS.GET,
-        headers: { Accept: 'application/json' },
-      }),
-    onSuccess: async (data) => {
-      const result = await data.json();
-      const filtered = result.filter((note: TNote) => note?.isShared);
-      dispatch(setShared(filtered));
-    },
-  });
+  useGetSharedNotes();
 
   return (
     <Box>
