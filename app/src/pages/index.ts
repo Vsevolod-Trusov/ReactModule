268e154b4e8e5b0c1a5e719289fc6aa9cc291c8a
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-13 03:26:17 +0300

    fix: change folder name

--- app/src/pages/index.ts
+++ app/src/pages/index.ts
@@ -2,7 +2,7 @@ import About from './About';
 import Error from './ErrorPage';
 import NoteList from './NoteList';
 import NotesForm from './NotesForm';
-import SharedNotes from './SharedNotes';
+import SharedNotes from './SharedNotesContainer';
 import EditNoteForm from './EditNoteForm';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
