ece5d16b365b1860cb045890d614b99382ee6a49
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-13 14:26:26 +0300

    fix: add api

--- app/src/pages/NoteList/NoteListContainer.tsx
+++ app/src/pages/NoteList/NoteListContainer.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { ROUTE } from 'config/constants/routes';
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'api/constants';
-import { setReduxNotes } from 'store/slices/notes.slice';
 import { selectFirstName } from 'store/slices/user.slice';
-import { QUERY_KEYS } from 'pages/constants';
+import { useGetNotes } from 'api/notes';
 
 import { TNote } from './types';
 import NoteList from './NoteList';
@@ -15,26 +12,8 @@ import NoteList from './NoteList';
 const NoteListContainer: FC = () => {
   const firstname = useSelector(selectFirstName);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
-  useQuery({
-    queryKey: [QUERY_KEYS.NOTES],
-    queryFn: async () =>
-      await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}`, {
-        method: FETCH_METHODS.GET,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        cache: 'no-store',
-      }),
-    onSuccess: async (data) => {
-      const notes = await data.json();
-      dispatch(
-        setReduxNotes(notes.filter((item: TNote) => item.author === firstname)),
-      );
-    },
-  });
+  useGetNotes();
 
   if (!window.localStorage.getItem('email') || !firstname) {
     return <Navigate to={ROUTE.SIGNIN} />;
