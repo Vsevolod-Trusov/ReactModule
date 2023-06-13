43d61052398b09f0829c80e21846804622b1caa7
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-13 14:28:29 +0300

    fix: add api

--- app/src/pages/SignUp/SignUpContainer.tsx
+++ app/src/pages/SignUp/SignUpContainer.tsx
@@ -1,26 +1,14 @@
 import React, { FC } from 'react';
-import { useMutation } from '@tanstack/react-query';
 import { FormikValues } from 'formik';
-import { useNavigate } from 'react-router-dom';
 
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'api/constants';
 import { Constants } from 'validations/constants';
 import { stringIsEquals } from 'utils/formatText';
-import { ROUTE } from 'config/constants/routes';
+import { useSignUp } from 'api/auth';
 
 import SignUp from './SignUp';
-import { IUser } from './types';
 
 const SignUpContainer: FC = () => {
-  const navigate = useNavigate();
-  const mutation = useMutation({
-    mutationFn: async (newUser: IUser) =>
-      await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.USERS}`, {
-        method: FETCH_METHODS.POST,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newUser),
-      }),
-  });
+  const mutation = useSignUp();
 
   const submit = (values: FormikValues): void => {
     if (stringIsEquals(values.password, values.confirmPassword)) {
@@ -35,14 +23,6 @@ const SignUpContainer: FC = () => {
       alert(Constants.PASSWORD_NOT_EQUAL);
     }
   };
-
-  if (mutation.isSuccess) {
-    navigate(ROUTE.SIGNIN);
-  }
-
-  if (mutation.isError) {
-    alert('error');
-  }
 
   return <SignUp submit={submit} />;
 };
