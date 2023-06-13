a8e93822494958887affd9d78f22ae92d7c93eb4
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-13 14:23:28 +0300

    fix: add in api axios request

--- app/src/components/FilterNotes/FilterNotesContainer.tsx
+++ app/src/components/FilterNotes/FilterNotesContainer.tsx
@@ -1,47 +1,18 @@
 import React, { FC } from 'react';
 import Box from '@mui/material/Box';
 import { FormikValues } from 'formik';
-import { useMutation } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 
-import { setReduxNotes, setShared } from 'store/slices/notes.slice';
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'api/constants';
-import { QUERY_KEYS } from 'pages/constants';
-import { TNote } from 'pages/NoteList/types';
-
 import FilterNotes from './FilterNotes';
-import { DATE_CREATION } from './constants';
 import { IFilterProps } from './types';
+import { useFilterNotes } from 'api/notes';
 
 const FilterNotesContainer: FC<IFilterProps> = ({ isShared }) => {
   const dispatch = useDispatch();
-  const url = new URL(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}`);
-
-  const mutation = useMutation({
-    mutationKey: [QUERY_KEYS.FILTER_NOTES],
-
-    mutationFn: async () =>
-      await fetch(url, {
-        method: FETCH_METHODS.GET,
-        headers: { Accept: 'application/json' },
-        cache: 'no-cache',
-      }),
-    onSuccess: async (data) => {
-      const notes = await data.json();
-      dispatch(
-        isShared
-          ? setShared(notes.filter((note: TNote) => note?.isShared))
-          : setReduxNotes(notes),
-      );
-    },
-  });
+  const mutation = useFilterNotes(isShared);
 
   const filter = (values: FormikValues) => {
-    url.searchParams.append(
-      DATE_CREATION,
-      new Date(values.dateCreation).toISOString(),
-    );
-    mutation.mutate();
+    mutation.mutate(values.dateCreation);
   };
 
   return (
