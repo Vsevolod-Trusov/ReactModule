71c759890cb625094121323cb0df4fbcbdda8a4b
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-14 15:53:53 +0300

    fix: add storing for selected note in redux

--- app/src/store/slices/notes.slice.ts
+++ app/src/store/slices/notes.slice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { INITIAL_STATE } from 'pages/NoteList/constants';
+
 import { RootState } from '../store';
 
 export const notesSlice = createSlice({
@@ -8,11 +10,16 @@ export const notesSlice = createSlice({
     notes: [],
     postNotes: [],
     shared: [],
+    selectedNote: INITIAL_STATE,
   },
   reducers: {
     setNotes: (state, { payload }) => ({ ...state, notes: payload }),
     setPostNotes: (state, { payload }) => ({ ...state, postNotes: payload }),
     setShared: (state, { payload }) => ({ ...state, shared: payload }),
+    setSelectedNote: (state, { payload }) => ({
+      ...state,
+      selectedNote: payload,
+    }),
   },
 });
 
@@ -20,8 +27,10 @@ export const {
   setNotes: setReduxNotes,
   setShared,
   setPostNotes,
+  setSelectedNote,
 } = notesSlice.actions;
 export const selectNotes = (state: RootState) => state.notes.notes;
 export const selectShared = (state: RootState) => state.notes.shared;
 export const selectPostNotes = (state: RootState) => state.notes.postNotes;
+export const selectNote = (state: RootState) => state.notes.selectedNote;
 export default notesSlice.reducer;
