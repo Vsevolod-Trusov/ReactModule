e57f4b801b55eb35e48bf32e1a2e28cf970ffbc7
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-14 15:51:50 +0300

    fix: add routes

--- app/src/config/constants/routes.ts
+++ app/src/config/constants/routes.ts
@@ -1,12 +1,16 @@
 export enum ROUTE {
   MAIN = '/',
   NOTES = '/notes',
+  SELECTED_NOTE = '/notes/selected',
   NOTES_FORM = '/notes-form',
   SHARED = '/shared-notes',
+  SELECTED_SHARE = '/shared-notes/selected',
   ABOUT = '/about',
   NOTE = '/note',
   SIGNIN = '/sign-in',
   SIGNUP = '/sign-up',
   NOT_FOUND = '/not-found',
+  MY_NOTE = '/notes/note',
+  SHARED_NOTE = '/shared-notes/note',
   ANY = '*',
 }
