f2f3b79f89c0c51d4888f3775c6753b4b4edef1d
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-14 15:50:21 +0300

    fix: change link

--- app/src/components/Navbar/components/Link/LinkContainer.tsx
+++ app/src/components/Navbar/components/Link/LinkContainer.tsx
@@ -1,13 +1,22 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { ILinkContainer } from 'components/Navbar/types';
 import { ROUTE } from 'config/constants/routes';
+import { INITIAL_STATE } from 'pages/NoteList/constants';
+import { setSelectedNote } from 'store/slices/notes.slice';
 
 import Link from './Link';
 
-const LinkContainer: FC<ILinkContainer> = ({ name, route, isExit }) => {
+const LinkContainer: FC<ILinkContainer> = ({
+  name,
+  route,
+  isExit,
+  isNoteLink,
+}) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   if (isExit) {
     return (
@@ -23,7 +32,13 @@ const LinkContainer: FC<ILinkContainer> = ({ name, route, isExit }) => {
 
   return (
     <Link
-      onClick={() => navigate(route ? route : ROUTE.NOT_FOUND)}
+      onClick={() => {
+        if (isNoteLink) {
+          localStorage.removeItem('selected');
+          dispatch(setSelectedNote(INITIAL_STATE));
+        }
+        navigate(route ? route : ROUTE.NOT_FOUND);
+      }}
       name={name}
     />
   );
