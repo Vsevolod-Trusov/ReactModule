b49e2a375aef586450c2b275c16361c7c37dfe29
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-14 15:49:08 +0300

    fix: change for shared notes too

--- app/src/api/notes/useEditNote.tsx
+++ app/src/api/notes/useEditNote.tsx
@@ -4,22 +4,36 @@ import {
   useQueryClient,
 } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { TNote, TUpdateNote } from 'pages/NoteList/types';
 import { QUERY_KEYS } from 'pages/constants';
 import { ROUTE } from 'config/constants/routes';
+import { setSelectedNote } from 'store/slices/notes.slice';
+import { INITIAL_STATE } from 'pages/NoteList/constants';
 
 import { TResponseError } from '../types';
 import { apiClient } from '../base';
 import { FETCH_URLS } from '../constants';
 
-export const useEditNote = (id): UseMutationResult<TNote[], TResponseError> => {
+export const useEditNote = (
+  id,
+  isShared,
+): UseMutationResult<TNote[], TResponseError> => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
 
   const handleSuccess = () => {
     queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.NOTES] });
-    navigate(ROUTE.NOTES);
+    localStorage.removeItem('selected');
+    dispatch(setSelectedNote(INITIAL_STATE));
+
+    if (isShared) {
+      navigate(ROUTE.SHARED);
+    } else {
+      navigate(ROUTE.NOTES);
+    }
   };
 
   return useMutation({
