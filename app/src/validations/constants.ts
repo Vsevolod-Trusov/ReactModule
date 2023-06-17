91b2419cde53404391b9d31ae601318ebdf0f9ab
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-17 08:22:11 +0300

    fix: change errors text to do shorter it

--- app/src/validations/constants.ts
+++ app/src/validations/constants.ts
@@ -3,11 +3,11 @@ export enum Constants {
   EMAIL_VALIDATION = 'Email is not valid',
 
   PASSWORD_REQUIRED = 'Password is required',
-  PASSWORD_VALIDATION = 'Password contains min 8 characters',
+  PASSWORD_VALIDATION = 'Min 8 characters',
 
   FIRST_NAME_REQUIRED = 'First name is required',
-  FIRST_NAME_VALIDATION = 'First name contains min 3 characters',
-  LAST_NAME_VALIDATION = 'Last name contains min 3 characters',
+  FIRST_NAME_VALIDATION = 'Min 3 characters',
+  LAST_NAME_VALIDATION = 'Min 3 characters',
   LAST_NAME_REQUIRED = 'Last name is required',
   BIRTHDAY_REQUIRED = 'Birthday is required',
   BIRTHDAY_VALIDATION = 'Birthday is not valid',
