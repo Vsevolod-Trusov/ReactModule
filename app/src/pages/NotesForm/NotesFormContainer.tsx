967fc36c3fad0c734206229a9c6dfd8ee1f2ec76
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-06-17 08:20:34 +0300

    fix: use  create hook

--- app/src/pages/NotesForm/NotesFormContainer.tsx
+++ app/src/pages/NotesForm/NotesFormContainer.tsx
@@ -1,32 +1,21 @@
 import React, { FC } from 'react';
-import { useMutation } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import { FormikValues } from 'formik';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { selectFirstName } from 'store/slices/user.slice';
-import { ROUTE } from 'config/constants/routes';
-import { FETCH_METHODS, FETCH_URLS, MOCK_API_ADDRESS } from 'api/constants';
-import { TNote } from 'pages/NoteList/types';
+import { useCreateNote } from 'api/notes';
 
 import NotesForm from './NotesForm';
 
 const NotesFormContainer: FC = () => {
-  const navigate = useNavigate();
   const firstname = useSelector(selectFirstName);
 
-  const mutation = useMutation({
-    mutationFn: async (note: TNote) =>
-      await fetch(`${MOCK_API_ADDRESS}${FETCH_URLS.NOTES}`, {
-        method: FETCH_METHODS.POST,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(note),
-      }),
-    onSuccess: () => navigate(ROUTE.NOTES),
-  });
+  const createNote = useCreateNote();
 
   const submit = (values: FormikValues): void => {
-    mutation.mutate({
+    createNote.mutate({
       id: null,
       title: values.title,
       description: values.description,
