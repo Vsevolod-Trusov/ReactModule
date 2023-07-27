6daffc8ba5e24f444bc582d307a354d19b9980c2
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-27 20:43:39 +0300

    fix: remove interceptors

--- app/src/api/base.ts
+++ app/src/api/base.ts
@@ -1,43 +1,8 @@
 import axios from 'axios';
 
 import { MOCK_API_ADDRESS } from './constants';
-import {
-  ACCESS_TOKEN,
-  REFRESH_TOKEN,
-  X_ACCESS_TOKEN,
-  X_REFRESH_TOKEN,
-} from './auth/constants';
 
 export const apiClient = axios.create({
-  baseURL: MOCK_API_ADDRESS, //,
+  baseURL: MOCK_API_ADDRESS,
   withCredentials: true,
 });
-
-apiClient.interceptors.request.use(
-  (request) => {
-    request.headers[X_REFRESH_TOKEN] =
-      window.sessionStorage.getItem(REFRESH_TOKEN);
-    request.headers[X_ACCESS_TOKEN] =
-      window.sessionStorage.getItem(ACCESS_TOKEN);
-    return request;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
-apiClient.interceptors.response.use(
-  (response) => {
-    const { refreshToken, accessToken } = response.data;
-
-    if (refreshToken && accessToken) {
-      window.sessionStorage.setItem(REFRESH_TOKEN, refreshToken);
-      window.sessionStorage.setItem(ACCESS_TOKEN, accessToken);
-    }
-
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
