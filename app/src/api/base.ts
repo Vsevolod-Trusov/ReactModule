32cd6876d3416c1ccb266fc62878868e2c56b268
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-27 19:53:08 +0300

    fix: change interceptors

--- app/src/api/base.ts
+++ app/src/api/base.ts
@@ -9,7 +9,7 @@ import {
 } from './auth/constants';
 
 export const apiClient = axios.create({
-  baseURL: MOCK_API_ADDRESS, //'http://localhost:4000', //
+  baseURL: 'http://localhost:4000', //MOCK_API_ADDRESS,
   withCredentials: true,
 });
 
@@ -34,9 +34,6 @@ apiClient.interceptors.response.use(
       window.sessionStorage.setItem(REFRESH_TOKEN, refreshToken);
       window.sessionStorage.setItem(ACCESS_TOKEN, accessToken);
     }
-
-    const headers = response.headers;
-    console.log(headers);
 
     return response;
   },
