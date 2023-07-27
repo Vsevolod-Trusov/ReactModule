e400d0f68efc845e29b989e585d80f037b4c318f
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-27 12:44:06 +0300

    fix: trying cookie work

--- app/src/api/base.ts
+++ app/src/api/base.ts
@@ -9,7 +9,8 @@ import {
 } from './auth/constants';
 
 export const apiClient = axios.create({
-  baseURL: MOCK_API_ADDRESS,
+  baseURL: MOCK_API_ADDRESS, //'http://localhost:4000', //
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use(
@@ -33,6 +34,10 @@ apiClient.interceptors.response.use(
       window.sessionStorage.setItem(REFRESH_TOKEN, refreshToken);
       window.sessionStorage.setItem(ACCESS_TOKEN, accessToken);
     }
+
+    const headers = response.headers;
+    console.log(headers);
+
     return response;
   },
   (error) => {
